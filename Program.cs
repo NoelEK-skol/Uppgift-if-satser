8b0a7ae322c80e5d382a0083ad230c28f72326bc
NoelEK-skol <noek07001@utb.vaxjo.se>
2024-08-30 13:55:03 +0200

    Gjort färdigt uppgift
    Fråga om det är fint väder, med if satser

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+{
+    Console.WriteLine("Är det fint väder?");
+    string response = Console.ReadLine();
+    if (response == "j" || response =="J"){
+            Console.WriteLine("Vi går på picknick!");}
+
+        else if (response == "n" || response == "N"){
+            Console.WriteLine("Vi stannar inne och läser en bok!");
+           }
+        else{
+        Console.WriteLine("Jag förstår inte");}
+          }  
